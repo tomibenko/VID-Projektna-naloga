2cc6a9566acddab1a13347cde8a866770ab756f8
Tomi Benko <146958001+tomibenko@users.noreply.github.com>
2024-06-03 19:21:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ class TestProjekt(unittest.TestCase):
 
     def test_placeholder(self):
         # This is a placeholder test that doesn't do anything
-        model = projekt.create_model()
+       
         print("Model created successfully, but no actual test is performed.")
 
 if __name__ == '__main__':
