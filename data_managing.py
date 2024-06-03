734372c54f21c7233b5acb86b74e1426e2fa51e1
zanlukakralj <zan.kralj3@student.um.si>
2024-06-03 19:57:42 +0200

    KAN-24 Dodal augmentacijo

--- data_managing.py
+++ data_managing.py
@@ -1,5 +1,7 @@
 import os
 import cv2
+import random
+import numpy as np
 from pymongo import MongoClient
 from flask import Flask
 
@@ -40,3 +42,67 @@ def preprocess_images(input_dir='captured_images', output_dir='preprocessed_imag
         output_path = os.path.join(output_dir, img_name)
         cv2.imwrite(output_path, img)
         print(f'Preprocessed {img_name}')
+
+# 3. Augmentacija podatkov
+def augment_dataset(input_dir='preprocessed_images', output_dir='augmented_images'):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    for img_name in os.listdir(input_dir):
+        img_path = os.path.join(input_dir, img_name)
+        img = cv2.imread(img_path)
+        augmented_images = augment_image(img)
+        for i, aug_img in enumerate(augmented_images):
+            aug_img_path = os.path.join(output_dir, f'{img_name}_aug_{i}.jpg')
+            cv2.imwrite(aug_img_path, aug_img)
+            print(f'Augmented {img_name} as {img_name}_aug_{i}.jpg')
+
+def augment_image(img):
+    augmented_images = []
+    rows, cols = img.shape[:2]
+
+    # Flip horizontal
+    augmented_images.append(cv2.flip(img, 1))
+
+    # Rotate
+    M_rotate = cv2.getRotationMatrix2D((cols/2, rows/2), 10, 1)
+    augmented_images.append(cv2.warpAffine(img, M_rotate, (cols, rows)))
+
+    # Brightness adjustment
+    augmented_images.append(cv2.convertScaleAbs(img, alpha=1.5, beta=0))
+
+    # Gaussian noise
+    noise = np.random.normal(0, 25, img.shape).astype(np.uint8)
+    augmented_images.append(cv2.add(img, noise))
+
+    # Custom augmentations
+    # 1. Crop
+    start_row, start_col = int(rows * .1), int(cols * .1)
+    end_row, end_col = int(rows * .9), int(cols * .9)
+    cropped_img = img[start_row:end_row, start_col:end_col]
+    resized_cropped_img = cv2.resize(cropped_img, (cols, rows))
+    augmented_images.append(resized_cropped_img)
+
+    # 2. Salt and pepper noise
+    salt_pepper_noise = img.copy()
+    salt_pepper_prob = 0.02
+    for i in range(img.shape[0]):
+        for j in range(img.shape[1]):
+            rand = random.random()
+            if rand < salt_pepper_prob:
+                salt_pepper_noise[i][j] = 0
+            elif rand > 1 - salt_pepper_prob:
+                salt_pepper_noise[i][j] = 255
+    augmented_images.append(salt_pepper_noise)
+
+    # 3. Contrast adjustment
+    contrast_img = cv2.convertScaleAbs(img, alpha=2.0, beta=0)
+    augmented_images.append(contrast_img)
+
+    # 4. Random erasing
+    erasing_img = img.copy()
+    x, y = random.randint(0, cols - 50), random.randint(0, rows - 50)
+    w, h = random.randint(20, 50), random.randint(20, 50)
+    erasing_img[y:y+h, x:x+w] = 0
+    augmented_images.append(erasing_img)
+
+    return augmented_images
