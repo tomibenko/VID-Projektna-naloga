f7be3e0a173352d0dd695ea42b72a370f76497c8
zanlukakralj <zan.kralj3@student.um.si>
2024-06-03 20:25:41 +0200

    Pošiljanje in verifikacija kode

--- data_managing.py
+++ data_managing.py
@@ -1,9 +1,11 @@
 import os
+import os
 import cv2
-import random
 import numpy as np
+import random
+import requests
 from pymongo import MongoClient
-from flask import Flask, requests
+from flask import Flask, request, jsonify
 
 # Konfiguracija za MongoDB
 client = MongoClient('mongodb+srv://zanluka:g1NmZuoD4MHnACDp@razvojapkzainternet.tb9k65s.mongodb.net/')
@@ -123,3 +125,45 @@ def send_push_notification(token, message):
     }
     response = requests.post(url, headers=headers, json=payload)
     return response.json()
+
+
+@app.route('/send_verification', methods=['POST'])
+
+# Pošiljanje verifikacijske kode
+def send_verification():
+    data = request.json
+    user_id = data['user_id']
+    device_token = data['device_token']
+    
+    code = random.randint(100000, 999999)
+    verification_data = {
+        "user_id": user_id,
+        "code": code
+    }
+    
+    # Shranjevanje verifikacijske kode v MongoDB
+    verification_codes_col.update_one(
+        {"user_id": user_id}, 
+        {"$set": verification_data}, 
+        upsert=True
+    )
+    
+    message = f"Your verification code is {code}"
+    send_push_notification(device_token, message)
+    return jsonify({"message": "Verification code sent."}), 200
+
+@app.route('/verify_code', methods=['POST'])
+
+# Verifikacija kode
+def verify_code():
+    data = request.json
+    user_id = data['user_id']
+    code = int(data['code'])
+    
+    # Preverjanje kode v MongoDB
+    verification_entry = verification_codes_col.find_one({"user_id": user_id})
+    
+    if verification_entry and verification_entry['code'] == code:
+        return jsonify({"message": "Verification successful."}), 200
+    else:
+        return jsonify({"message": "Verification failed."}), 401
